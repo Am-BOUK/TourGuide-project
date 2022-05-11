76572341cc976ca7478f2287674f61739923ed38
Amal <ab.amal.boukili@gmail.com>
2022-05-11 22:51:04 +0200

    adding getAllCurrentLocationsTest and fixing getNearbyAttractionsTest

--- TourGuide/src/test/java/tourGuide/TestTourGuideService.java
+++ TourGuide/src/test/java/tourGuide/TestTourGuideService.java
@@ -1,38 +1,59 @@
 package tourGuide;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.UUID;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
 import rewardCentral.RewardCentral;
 import tourGuide.helper.InternalTestHelper;
 import tourGuide.service.RewardsService;
 import tourGuide.service.TourGuideService;
 import tourGuide.user.User;
+import tourGuide.user.UserReward;
 import tripPricer.Provider;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class TestTourGuideService {
-	@Autowired
+//	@Autowired
 	private TourGuideService tourGuideService;
+	private GpsUtil gpsUtil;
 
 	@Before
 	public void setUpTest() {
 		Locale.setDefault(Locale.US);
-		GpsUtil gpsUtil = new GpsUtil();
+		gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		InternalTestHelper.setInternalUserNumber(0);
 		tourGuideService = new TourGuideService(gpsUtil, rewardsService);
+	}
+
+	@Test
+	public void getUserRewardsTest() {
+		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+
+		Attraction attraction = gpsUtil.getAttractions().get(0);
+		user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
+		tourGuideService.trackUserLocation(user);
+		List<UserReward> getUserRewards = tourGuideService.getUserRewards(user);
+		assertEquals(1, getUserRewards.size());
 	}
 
 	@Test
@@ -62,8 +83,8 @@ public class TestTourGuideService {
 
 	@Test
 	public void getAllUsersTest() {
-		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
-		User user2 = new User(UUID.randomUUID(), "jon2", "000", "jon2@tourGuide.com");
+		User user = new User(UUID.randomUUID(), "jon3", "000", "jon@tourGuide.com");
+		User user2 = new User(UUID.randomUUID(), "jon4", "000", "jon2@tourGuide.com");
 
 		tourGuideService.addUser(user);
 		tourGuideService.addUser(user2);
@@ -87,11 +108,11 @@ public class TestTourGuideService {
 	}
 
 	@Test
-	public void getNearbyAttractions() {
+	public void getNearbyAttractionsTest() {
 		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
 		VisitedLocation visitedLocation = tourGuideService.trackUserLocation(user);
 
-		List<Attraction> attractions = tourGuideService.getNearByAttractions(visitedLocation);
+		Map<String, Map<String, Double>> attractions = tourGuideService.getNearByAttractions(visitedLocation);
 
 		tourGuideService.tracker.stopTracking();
 
@@ -109,4 +130,11 @@ public class TestTourGuideService {
 		assertEquals(5, providers.size());
 	}
 
+	@Test
+	public void getAllCurrentLocationsTest() {
+		Map<String, Location> getAllCurrentLocations = tourGuideService.getAllCurrentLocations();
+
+		tourGuideService.tracker.stopTracking();
+		assertNotNull(getAllCurrentLocations);
+	}
 }
